14ae59de6061c9f2bb94757318e881ae078a507b
Sphurti <sphurtisharmass@gmail.com>
2020-05-20 08:30:53 -0700

    class created

---
+++ src/myJavaProject/myAppTester.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package myJavaProject;
+
+/**
+ * @author Sphurti
+ *
+ */
+public class myAppTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
